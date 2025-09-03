a83597cdac18f5193402cb64de436260e6443a5d
tranbnn <tranbn@outlook.com>
2025-09-04 01:39:57 +1000

    idk

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,9 +2,7 @@
 
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 173:
-        return "True"
+        return True
 
-    elif ord(c) >= 65 and ord(c) <= 91:
-        return "False"
-    else:
-        return "idk"
+    else
+        return False
