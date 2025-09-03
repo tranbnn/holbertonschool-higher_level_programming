eaa4cf54cf7fdcc8bbebf7ddf1839ee383d5be0a
tranbnn <tranbn@outlook.com>
2025-09-04 00:58:21 +1000

    combos

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,7 @@ for i in range(10):
     for j in range(10):
         if i >= j:
             continue
-        print("{}{}, ".format(i, j), end="")
-    if i == 8 and j == 9:
-        print("{}{}".format(i, j))
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
