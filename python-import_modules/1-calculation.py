9ad2a9d15bc40e3893f205dca42cd0d850ee6744
tranbnn <tranbn@outlook.com>
2025-09-07 21:58:34 +1000

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,15 +3,11 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
 
 if __name__ == "__main__":
 	
 	
-	print("{} + {} = {}".format(a, b, add_result))
-	print("{} + {} = {}".format(a, b, sub_result))
-	print("{} + {} = {}".format(a, b, mul_result))
-	print("{} + {} = {}".format(a, b, div_result))
+	print("{} + {} = {}".format(a, b, add(a, b)))
+	print("{} - {} = {}".format(a, b, sub(a, b)))
+	print("{} * {} = {}".format(a, b, mul(a, b)))
+	print("{} / {} = {}".format(a, b, div(a, b)))
