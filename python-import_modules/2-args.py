7064041503acf9aa772999a05d6ffd668800b2ec
tranbnn <tranbn@outlook.com>
2025-09-07 23:06:54 +1000

    testing

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,11 @@ import sys
 if __name__ == "__main__":
 
     args = len(sys.argv) - 1
-    
     if args == 0:
         print("0 arguments.")
     elif args == 1:
         print("1 argument:")
     else:
         print(f"{args} arguments:")
-    
     for i in range(1, len(sys.argv)):
         print(f"{i}: {sys.argv[i]}")
