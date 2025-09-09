26b6895dbfc4939d7d8c312e9c49c240648d3ec2
tranbnn <tranbn@outlook.com>
2025-09-09 22:47:57 +1000

    find the max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if not my_list:
+        return None
+
+    biggest_int = my_list[0]
+
+    for i in my_list:
+        if i > biggest_int:
+            biggest_int = i
+
+    return biggest_int
