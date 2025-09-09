853bc426f40c5926f4862894bb6add2a2dca7cb5
tranbnn <tranbn@outlook.com>
2025-09-09 22:15:15 +1000

    add tuple

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    fixed_tuple_a = 0
+    fixed_tuple_n = 0
+
+    if len(tuple_a) < 2:
+        fixed_tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    else:
+        fixed_tuple_a = tuple_a
+
+    if len(tuple_b) <2:
+        fixed_tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+    else:
+        fixed_tuple_b = tuple_b
+
+    first_elements = fixed_tuple_a[0] + fixed_tuple_b[0]
+    second_elements = fixed_tuple_a[1] + fixed_tuple_b[1]
+
+    return (first_elements, second_elements)
