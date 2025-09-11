ba4633bf11b98db8520be0804dde986cc38cd4be
tranbnn <tranbn@outlook.com>
2025-09-11 13:44:06 +1000

    only diff

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-
-    combined_set = (set_1.union(set_2))
-    set(combined_set)
-
-    return combined_set
+    set_3 = set_1. symmetric_difference(set_2)
+    return set_3
 
