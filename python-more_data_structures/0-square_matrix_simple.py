66ec9c4b698aef83b22d1a029a8cb40109d1557a
tranbnn <tranbn@outlook.com>
2025-09-11 11:31:48 +1000

    test

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    copy_matrix = matrix
+    squared_matrix_max = map(square, copy_matrix)
+    new_matrix = list(squared_marix_max)
+
+    return new_matrix
