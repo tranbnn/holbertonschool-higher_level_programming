5f7fd06639c0a9412b9760593b1083bf2cbacfcc
tranbnn <tranbn@outlook.com>
2025-09-11 17:47:23 +1000

    sorted

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
 
     for key in sorted(a_dictionary):
-        print(f"{key}: {a_dictionary[key]})")
+        print(f"{key}: {a_dictionary[key]}")
     return a_dictionary
