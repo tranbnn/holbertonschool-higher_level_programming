bcaeebb134db1df32c510d5d030a88445871cbcf
tranbnn <tranbn@outlook.com>
2025-09-11 12:44:40 +1000

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,12 @@
 
 def search_replace(my_list, search, replace):
 
+    new_list = []
+
     for i in my_list:
         if i == search:
-            replace
+            new_list.append(replace)
+        else:
+            new_list.append(search)
+
+    return new_list
