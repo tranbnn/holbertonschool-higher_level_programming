5f867945fc062a0b1d7dcb972e42bfc65292c5ff
tranbnn <tranbn@outlook.com>
2025-09-13 13:28:24 +1000

    mult by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@ def multiply_by_2(a_dictionary):
 
     new_dictionary = {}
 
-    for key, value in a_dictionary.items():    
+    for key, value in a_dictionary.items():
         new_dictionary[key] = value * 2
 
     return new_dictionary
