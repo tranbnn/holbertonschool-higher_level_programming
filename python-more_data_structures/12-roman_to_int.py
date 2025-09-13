21e275812bfe3d8f34a79c30b9b6e15416353136
tranbnn <tranbn@outlook.com>
2025-09-13 17:07:33 +1000

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000}
+                 'C': 100, 'D': 500, 'M': 1000}
 
     int_val = 0
     prev_val = 0
