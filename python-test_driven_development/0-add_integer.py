a05e9b3c583c0aa582b71232c6d650a74b2f538b
tranbnn <tranbn@outlook.com>
2025-09-17 06:42:04 +1000

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
     into ints if not already.
     """
 
-    if not isinstance(a, (int,float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
