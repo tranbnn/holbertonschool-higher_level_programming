636429e11b5cdecb75e24b57d1ac009f5c5c4b72
tranbnn <tranbn@outlook.com>
2025-09-17 06:40:12 +1000

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 This module provides a function to add two integers.
 """
 
+
 def add_integer(a, b=98):
     """
     add_integer: Adds two integers
@@ -12,7 +13,8 @@ def add_integer(a, b=98):
         b: The second input integer, default is 98
 
     Returns:
-    The sum of a and b as ints. a and b will be casted into ints if not already.
+    The sum of a and b as ints. a and b will be casted
+    into ints if not already.
     """
 
     if not isinstance(a, (int,float)):
